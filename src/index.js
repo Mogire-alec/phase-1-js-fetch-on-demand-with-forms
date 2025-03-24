9890ebdc22b12b37bd7b4b28ca706f7ce6b2de71
Alex <alexandermogire009@gmail.com>
2025-03-24 17:13:17 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault()
+        console.log(event);
+    });
+}
+fetch(`http://localhost:3000/movies/`)
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error ("Movie not found");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          const title = document.querySelector("section#movieDetails h4");
+          const summary = document.querySelector("section#movieDetails p");
+  
+          title.innerText = data.title;
+          summary.innerText = data.summary;
+        })
+        .catch((error) => {
+          const title = document.querySelector("section#movieDetails h4");
+          const summary = document.querySelector("section#movieDetails p");
+          
+          title.innerText = "Error";
+          summary.innerText = error.message;
+        });
+    
+
+  document.addEventListener("DOMContentLoaded", init);
